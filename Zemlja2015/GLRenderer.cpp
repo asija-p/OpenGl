8a699c24886d8cea153342624e396e1f0ffa2a58
asija <156678440+asija-p@users.noreply.github.com>
2026-02-27 10:14:50 +0100

    Refactor GLRenderer for improved camera and lighting
    Refactor camera and lighting setup, update material properties, and streamline scene rendering.

--- Zemlja2015/GLRenderer.cpp
+++ Zemlja2015/GLRenderer.cpp
@@ -4,48 +4,26 @@
 #include "GL\glu.h"
 #include "GL\glaux.h"
 #include "GL\glut.h"
+#include "DImage.h"
 //#pragma comment(lib, "GL\\glut32.lib")
 
 CGLRenderer::CGLRenderer(void)
 {
 	m_hrc = NULL;
 
-	m_cameraR = 30.0;
-	m_cameraAngleXY = 0.0;
-	m_cameraAngleXZ = 0.0;
+	m_eyex = 0.0;
+	m_eyey = 0.0;
+	m_eyez = 0.0;
 
-	UpdateCameraPosition();
+	cameraR = 50;
+	cameraXY = 0;
+	cameraXZ = 0;
 
-	// 🔵 Plavi (tirkizni) prstenovi
-	m_blueMaterial.PostaviAmbijent(0.0f, 0.0f, 1.0f, 1.0f);
-	m_blueMaterial.PostaviDifuznu(0.0f, 0.0f, 1.0f, 1.0f);
-	m_blueMaterial.PostaviSpekularnu(1.0f, 1.0f, 1.0f, 1.0f);
-	m_blueMaterial.PostaviShininess(16.0f);
+	normale = false;
+	m_light = false;
+	moon_rot = 0;
 
-	// 🔴 Ljubičasti / crveni prstenovi
-	m_redMaterial.PostaviAmbijent(1.0f, 0.0f, 0.0f, 1.0f);
-	m_redMaterial.PostaviDifuznu(1.0f, 0.0f, 0.0f, 1.0f);
-	m_redMaterial.PostaviSpekularnu(1.0f, 1.0f, 1.0f, 1.0f);
-	m_redMaterial.PostaviShininess(16.0f);
-
-	// 🏛 Osnova (postolje)
-	m_pedestalMaterial.PostaviAmbijent(0.6f, 0.6f, 0.6f, 1.0f);
-	m_pedestalMaterial.PostaviDifuznu(0.8f, 0.8f, 0.8f, 1.0f);
-	m_pedestalMaterial.PostaviSpekularnu(0.5f, 0.5f, 0.5f, 1.0f);
-	m_pedestalMaterial.PostaviShininess(64.0f);
-
-	// 🏠 Soba (zidovi)
-	m_wallMaterial.PostaviAmbijent(0.1f, 0.1f, 0.1f, 1.0f);
-	m_wallMaterial.PostaviDifuznu(0.8f, 0.8f, 0.8f, 1.0f);
-	m_wallMaterial.PostaviSpekularnu(0.2f, 0.2f, 0.2f, 1.0f);
-	m_wallMaterial.PostaviEmisivnu(0.1f, 0.1f, 0.1f, 1.0f);
-	m_wallMaterial.PostaviShininess(0.0f);
-
-	m_blueLight.PostaviAmbijent(0, 1, 1, 1);
-	m_blueLight.PostaviDifuznu(0, 1, 1, 0);
-	m_blueLight.PostaviSpekularnu(0, 1, 1, 1);
-	m_blueLight.PostaviEmisivnu(0, 1, 1, 1);
-	m_blueLight.PostaviShininess(0);
+	CameraUpdate();
 }
 
 CGLRenderer::~CGLRenderer(void)
@@ -83,43 +61,69 @@ void CGLRenderer::PrepareScene(CDC *pDC)
 {
 	wglMakeCurrent(pDC->m_hDC, m_hrc);
 	//---------------------------------
-	glClearColor(0.4f, 0.7f, 1.0f, 1.0f);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_NORMALIZE);
+
+	TSC[0] = LoadTexture("TSC0.jpg");
+	TSC[1] = LoadTexture("TSC1.jpg");
+	TSC[2] = LoadTexture("TSC2.jpg");
+	TSC[3] = LoadTexture("TSC3.jpg");
+	TSC[4] = LoadTexture("TSC4.jpg");
+	TSC[5] = LoadTexture("TSC5.jpg");
+
+	M[0] = LoadTexture("M0.jpg");
+	M[1] = LoadTexture("M1.jpg");
+	M[2] = LoadTexture("M2.jpg");
+	M[3] = LoadTexture("M3.jpg");
+	M[4] = LoadTexture("M4.jpg");
+	M[5] = LoadTexture("M5.jpg");
+
+	S[0] = LoadTexture("S0.jpg");
+	S[1] = LoadTexture("S1.jpg");
+	S[2] = LoadTexture("S2.jpg");
+	S[3] = LoadTexture("S3.jpg");
+	S[4] = LoadTexture("S4.jpg");
+	S[5] = LoadTexture("S5.jpg");
+
+	glEnable(GL_TEXTURE_2D);
+
 	//---------------------------------
 	wglMakeCurrent(NULL, NULL);
 }
 
-void CGLRenderer::DrawScene(CDC *pDC)
+void CGLRenderer::DrawScene(CDC* pDC)
 {
 	wglMakeCurrent(pDC->m_hDC, m_hrc);
 	//---------------------------------
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	gluLookAt(
-		m_eyex, m_eyey, m_eyez,
-		0.0, 10.0, 0.0,
-		0.0, 1.0, 0.0
-	);
+	gluLookAt(m_eyex, m_eyey, m_eyez,
+		0, 0, 0,
+		0, 1, 0);
+	DrawTriangle();
 
-	SetUpAmbient();
-	SetUpDirection();
-	PositionalLight1();
+	GLfloat light_position[] = { 0.0, 0.0, 1.0, 0.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
-	glEnable(GL_LIGHTING);
-	
-	DrawAxis(20);
-	DrawWalls();
+	glDisable(GL_LIGHTING);
 
-	glPushMatrix();
-	glRotatef(45, 0, 1, 0);
-	DrawPedestal();
-	glPopMatrix();
+	DrawSpace(100, 1);
+
+	if (m_light)
+	{
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+	}
+
+	DrawEarth(10, 1);
+	glRotatef(moon_rot, 0, 1, 0);
+	glTranslatef(0, 0, 50);
+	DrawMoon(3, 1);
 
 	glFlush();
-	//---------------------------------
 	SwapBuffers(pDC->m_hDC);
+	//---------------------------------
 	wglMakeCurrent(NULL, NULL);
 }
 
@@ -128,10 +132,11 @@ void CGLRenderer::Reshape(CDC *pDC, int w, int h)
 	wglMakeCurrent(pDC->m_hDC, m_hrc);
 	//---------------------------------
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	double aspect = (double)w / (double)h;
 	glMatrixMode(GL_PROJECTION);
+
 	glLoadIdentity();
-	gluPerspective(45.0, aspect, 1, 500);
+
+	gluPerspective(40, (double)w / (double)h, 0.1, 200);
 	glMatrixMode(GL_MODELVIEW);
 	//---------------------------------
 	wglMakeCurrent(NULL, NULL);
@@ -141,6 +146,11 @@ void CGLRenderer::DestroyScene(CDC *pDC)
 {
 	wglMakeCurrent(pDC->m_hDC, m_hrc);
 	// ... 
+
+	glDeleteTextures(6, TSC);
+	glDeleteTextures(6, M);
+	glDeleteTextures(6, S);
+
 	wglMakeCurrent(NULL,NULL); 
 	if(m_hrc) 
 	{
@@ -149,409 +159,231 @@ void CGLRenderer::DestroyScene(CDC *pDC)
 	}
 }
 
-void CGLRenderer::UpdateCameraPosition()
+UINT CGLRenderer::LoadTexture(char* fileName)
 {
-	double radXY = ToRad(m_cameraAngleXY);
-	double radXZ = ToRad(m_cameraAngleXZ);
+	UINT texId;
+	DImage img;
+	img.Load(CString(fileName));
 
-	m_eyex = m_cameraR * cos(radXZ) * cos(radXY);
-	m_eyey = m_cameraR * sin(radXZ);
-	m_eyez = m_cameraR * cos(radXZ) * sin(radXY);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	glGenTextures(1, &texId);
+	glBindTexture(GL_TEXTURE_2D, texId);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, img.Width(), img.Height(), GL_BGRA_EXT, GL_UNSIGNED_BYTE, img.GetDIBBits());
+
+	return texId;
+;
 }
 
-void CGLRenderer::RotateView(double dx, double dy)
+void CGLRenderer::DrawTriangle()
 {
-	m_cameraAngleXY -= dx;
-	m_cameraAngleXZ += dy;
+	glDisable(GL_TEXTURE_2D);
 
-	if (m_cameraAngleXZ > 90) m_cameraAngleXZ = 90;
-	if (m_cameraAngleXZ < -90) m_cameraAngleXZ = -90;
+	glBegin(GL_TRIANGLES);
 
-	UpdateCameraPosition();
-}
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(1, 0, 0);
 
-void CGLRenderer::ZoomView(double r)
-{
-	m_cameraR += r;
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(0, 1, 0);
 
-	UpdateCameraPosition();
-}
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0, 0, 1);
 
-void CGLRenderer::DrawAxis(double width)
-{
-	glColor3f(1, 0, 0);
-	glBegin(GL_LINES);
-	glVertex3f(0, 0, 0);
-	glVertex3f(width, 0, 0);
 	glEnd();
 
-	glColor3f(0, 1, 0);
-	glBegin(GL_LINES);
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, width, 0);
-	glEnd();
-
-	glColor3f(0, 0, 1);
-	glBegin(GL_LINES);
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, 0, width);
-	glEnd();
-
+	glEnable(GL_TEXTURE_2D);
 }
 
-void CGLRenderer::DrawWalls()
+void CGLRenderer::DrawPatch(double R, int n)
 {
-	m_wallMaterial.IzaberiMaterijal();
+	float step = 2.0 / n;
 
-	int sides = 4;
-	int length = 80;
-	int height = 80;
-	int step = 1;
-	double angle = 0;
-	double r = (sqrt(pow(length, 2) + pow(length, 2))) / 2; double dAlpha = ToRad(360.0 / sides);
-	int half = length / 2;
-
-	glPushMatrix();
-
-	for (int side = 0; side < 4; side++)
+	for (float i = -1; i < 1; i += step) //y
 	{
-
-		glNormal3f(0, 0, -1);
-
-		for (int i = 0; i < height; i += step)
+		float i2 = i + step;
+		glBegin(GL_TRIANGLE_STRIP);
+		for (float j = -1; j <= 1; j += step) //x
 		{
-			glBegin(GL_QUAD_STRIP);
-			for (int j = -half; j <= half; j += step)
-			{
-				glVertex3f(j, i, half);
-				glVertex3f(j, i + step, half);
-			}
-			glEnd();
-		}
+			float fi = atan(j);
+			float teta = atan(i * cos(fi));
 
-		glRotatef(90, 0, 1, 0);
-	}
+			double x1 = R * cos(teta) * sin(fi);
+			double z1 = R * cos(teta) * cos(fi);
+			double y1 = R * sin(teta);
 
-	glPopMatrix();
+			glTexCoord2f((j + 1) / 2, 1.0 -(i + 1) / 2.0);
+			glNormal3f(x1 / R, y1 / R, z1 / R);
+			glVertex3f(x1, y1, z1);
 
-	//top
-	glNormal3f(0, -1, 0);
-	for (int i =-half; i <=half; i += step)
-	{
-		glBegin(GL_QUAD_STRIP);
-		for (int j = -half; j <= half; j+=step)
-		{
-			glVertex3f(j, length, i);
-			glVertex3f(j, length, i+step);
+			float teta2 = atan(i2 * cos(fi));
 
+			double x2 = R * cos(teta2) * sin(fi);
+			double z2 = R * cos(teta2) * cos(fi);
+			double y2 = R * sin(teta2);
+
+			glTexCoord2f((j + 1) / 2, 1.0 - (i2 + 1) / 2.0);
+			glNormal3f(x2 / R, y2 / R, z2 / R);
+			glVertex3f(x2, y2, z2);
 		}
 		glEnd();
-	}
-	
-
-	//bottom
-	glNormal3f(0, 1, 0);
-	for (int i = -half; i <= half; i += step)
-	{
-		glBegin(GL_QUAD_STRIP);
-		for (int j = -half; j <= half; j += step)
-		{
-			glVertex3f(j, 0, i);
-			glVertex3f(j, 0, i+step);
-
-		}
-		glEnd();
-	}
-
-}
-
-void CGLRenderer::DrawPedestal()
-{
-	m_pedestalMaterial.IzaberiMaterijal();
-
-	DrawHalfSphere();
-	glTranslatef(0, 4.5, 0);
-	DrawCylinder(2, 8, 3);
-	glTranslatef(0, 3, 0);
-	DrawCylinder(7, 4, 2);
-	glTranslatef(0, 2, 0);
-
-	DrawRing(4, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 2, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(2, 2, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(2, 2, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(2, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 2, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(2, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 4, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(4, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 4, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(4, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 2, true);
-	glTranslatef(0, 1, 0);
-	DrawRing(2, 3, false);
-	glTranslatef(0, 1, 0);
-	DrawRing(3, 4, true);
-	
-}
-
-void CGLRenderer::DrawCylinder(int r, int nSeg, int h)
-{
-
-	double dAlpha = ToRad(360.0 / nSeg);
-
-	glBegin(GL_QUAD_STRIP);
-	for (int i = 0; i <= nSeg; i++)
-	{
-		double angle = i * dAlpha;
-		double x = r * cos(angle);
-		double z = r * sin(angle);
-
-		glNormal3f(cos(angle), 0, sin(angle));
-		glVertex3f(x, h, z);
-		glVertex3f(x, 0, z);
-	}
-	glEnd();
-
-	glBegin(GL_TRIANGLE_FAN);
-	glNormal3f(0, -1, 0);
-	glVertex3f(0, 0, 0);
-	for (int i = 0; i <= nSeg; i++)
-	{
-		double x = r * cos(i * dAlpha);
-		double z = r * sin(i * dAlpha);
-
-		glVertex3f(x, 0, z);
-	}
-	glEnd();
-
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_TRIANGLE_FAN);
-	glNormal3f(0, 1, 0);
-	glVertex3f(0, h, 0);
-	for (int i = 0; i <= nSeg; i++)
-	{
-		double x = r * cos(i * dAlpha);
-		double z = r * sin(i * dAlpha);
-
-		glVertex3f(x, h, z);
-	}
-	glEnd();
-}
-
-void CGLRenderer::DrawHalfSphere()
-{
-	int nSegAlpha = 40;
-	int nSegBeta = 40;
-	int r = 5;
-
-	double dAlpha = ToRad(90.0 / nSegAlpha);
-	double dBeta = ToRad(360.0 / nSegBeta);
-
-	for (int i = 0; i < nSegAlpha; i++)
-	{
-		glBegin(GL_QUAD_STRIP);
-		for (int j = 0; j <= nSegBeta; j++)
-		{
-			double x = r * cos(dAlpha * (i + 1)) * cos(dBeta * j);
-			double y = r * sin(dAlpha * (i + 1));
-			double z = r * cos(dAlpha * (i + 1)) * sin(dBeta * j);
-
-			glNormal3f(x / r, y / r, z / r);
-			glVertex3f(x, y, z);
-
-			x = r * cos(dAlpha * i) * cos(dBeta * j);
-			y = r * sin(dAlpha * i);
-			z = r * cos(dAlpha * i) * sin(dBeta * j);
-
-			glNormal3f(x / r, y / r, z / r);
-			glVertex3f(x, y, z);
-		}
-		glEnd();
-	}
-}
-
-void CGLRenderer::DrawRing(int r1, int r2, bool red)
-{
-	if (red)
-		m_redMaterial.IzaberiMaterijal();
-	else
-		m_blueMaterial.IzaberiMaterijal();
-
-	int nSeg = 20;
-	double dBeta = ToRad(360.0 / nSeg);
-
-	int h = 1;
-
-	double r = abs(r1 - r2);
-	double L = sqrt(pow(r, 2) + pow(h, 2));
-	double ny = r / L;
-	double nr = h / L;
-	int v = r2 > r1 ? -1 : 1;
-
-	glBegin(GL_QUAD_STRIP);
-	for (int i = 0; i <= nSeg; i++)
-	{
-		double angle = i * dBeta;
-
-		double x1 = r1 * cos(0) * cos(angle);
-		double y1 = r1 * sin(0);
-		double z1 = r1 * cos(0) * sin(angle);
-
-		double x2 = r2 * cos(0) * cos(angle);
-		double y2 = r2 * sin(0);
-		double z2 = r2 * cos(0) * sin(angle);
-
-
-		double nx = nr * cos(angle);
-		double nz = nr * sin(angle);
-		
-		glNormal3f(nx, ny, nz);
-		glVertex3f(x1, y1, z1);
-		glVertex3f(x2, y2 + h, z2);
-	}
-	glEnd();
+	}	
 
 	if (normale)
 	{
-		glColor3f(0, 1, 0);
-		glBegin(GL_LINES);
-		for (int i = 0; i <= nSeg; i++)
+		glDisable(GL_TEXTURE_2D);
+		for (float i = -1; i < 1; i += step) //y
 		{
-			double angle = i * dBeta;
+			float i2 = i + step;
+			glBegin(GL_LINES);
 
-			double x1 = r1 * cos(0) * cos(angle);
-			double y1 = r1 * sin(0);
-			double z1 = r1 * cos(0) * sin(angle);
+			glColor3f(1, 1, 0);
+			for (float j = -1; j <= 1; j += step) //x
+			{
+				float fi = atan(j);
+				float teta = atan(i * cos(fi));
 
-			double x2 = r2 * cos(0) * cos(angle);
-			double y2 = r2 * sin(0);
-			double z2 = r2 * cos(0) * sin(angle);
+				double x1 = R * cos(teta) * sin(fi);
+				double z1 = R * cos(teta) * cos(fi);
+				double y1 = R * sin(teta);
 
+				glVertex3f(x1, y1, z1);
+				float len = R * 0.2f;
+				glVertex3f(x1 + (x1 / R) * len, y1 + (y1 / R) * len, z1 + (z1 / R) * len);
 
-			double nx = nr * cos(angle);
-			double nz = nr * sin(angle);
+				float teta2 = atan(i2 * cos(fi));
 
-			glVertex3f(x1, y1, z1);
-			glVertex3f(x1+nx, y1+ny*v, z1+nz);
+				double x2 = R * cos(teta2) * sin(fi);
+				double z2 = R * cos(teta2) * cos(fi);
+				double y2 = R * sin(teta2);
 
-			glVertex3f(x2, y2 + h, z2);
-			glVertex3f(x2+nx, y2 + h+ny*v, z2+nz);
+				glVertex3f(x2, y2, z2);
+				glVertex3f(x2 + (x2 / R) * len, y2 + (y2 / R) * len, z2 + (z2 / R) * len);
+			}
+			glEnd();
 		}
-		glEnd();
+		glEnable(GL_TEXTURE_2D);
 	}
 }
 
-void CGLRenderer::ShowNormale()
+void CGLRenderer::DrawEarth(double R, int tes)
 {
-	normale = !normale;
+	glBindTexture(GL_TEXTURE_2D, TSC[0]);
+	glColor3f(1.0, 1.0, 1.0);
+	DrawPatch(R, 10);
+
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, TSC[1]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+
+	glBindTexture(GL_TEXTURE_2D, TSC[2]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+
+	glBindTexture(GL_TEXTURE_2D, TSC[3]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
+
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, TSC[4]);
+	glRotatef(-90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
+
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, TSC[5]);
+	glRotatef(90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
+
 }
 
-void CGLRenderer::SetUpAmbient()
+void CGLRenderer::DrawMoon(double R, int tes)
 {
-	GLfloat ambijentalno_svetlo[] = { 0.4f, 0.4f, 0.4f, 1.0f };
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambijentalno_svetlo);
+	glBindTexture(GL_TEXTURE_2D, M[0]);
+	glColor3f(1.0, 1.0, 1.0);
+	DrawPatch(R, 10);
 
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, M[1]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+
+	glBindTexture(GL_TEXTURE_2D, M[2]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+
+	glBindTexture(GL_TEXTURE_2D, M[3]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
+
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, M[4]);
+	glRotatef(-90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
+
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, M[5]);
+	glRotatef(90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
 }
 
-void CGLRenderer::SetUpDirection()
+void CGLRenderer::DrawSpace(double R, int tes)
 {
-	GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };		// Ambijentalna komponenta izvora
-	GLfloat light_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };		// Jaka bela svetlost
-	GLfloat light_specular[] = { 0.8f, 0.8f, 0.8f, 1.0f };		// Beli odsjaj
+	glBindTexture(GL_TEXTURE_2D, S[0]);
+	glColor3f(1.0, 1.0, 1.0);
+	DrawPatch(R, 10);
 
-	GLfloat light_position[] = { 13.0f, 39.0f, 13.0f, 0.0f };
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, S[1]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
 
-	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
-	glLightfv(GL_LIGHT1, GL_POSITION, light_position);
+	glBindTexture(GL_TEXTURE_2D, S[2]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
 
-	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 180.0f);
+	glBindTexture(GL_TEXTURE_2D, S[3]);
+	glRotatef(90, 0, 1, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
 
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, S[4]);
+	glRotatef(-90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
 
-	glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 1.0f);
-	glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.0f);
-	glLightf(GL_LIGHT1, GL_QUADRATIC_ATTENUATION, 0.0f);
-
-	glEnable(GL_LIGHT1);
+	glPushMatrix();
+	glBindTexture(GL_TEXTURE_2D, S[5]);
+	glRotatef(90, 1, 0, 0);
+	DrawPatch(R, 10);
+	glPopMatrix();
 }
 
-void CGLRenderer::DrawSphere(int r)
+void CGLRenderer::CameraUpdate()
 {
-	int nSegAlpha = 40;
-	int nSegBeta = 40;
+	if (cameraXY > 90) cameraXY = 90;
+	if (cameraXY < -90) cameraXY = -90;
 
-	double dAlpha = ToRad(1800.0 / nSegAlpha);
-	double dBeta = ToRad(360.0 / nSegBeta);
+	double alpha = ToRad(cameraXY);
+	double beta = ToRad(cameraXZ);
 
-	for (int i = 0; i < nSegAlpha; i++)
-	{
-		glBegin(GL_QUAD_STRIP);
-		for (int j = 0; j <= nSegBeta; j++)
-		{
-			double x = r * cos(dAlpha * (i + 1)) * cos(dBeta * j);
-			double y = r * sin(dAlpha * (i + 1));
-			double z = r * cos(dAlpha * (i + 1)) * sin(dBeta * j);
-
-			glNormal3f(x / r, y / r, z / r);
-			glVertex3f(x, y, z);
-
-			x = r * cos(dAlpha * i) * cos(dBeta * j);
-			y = r * sin(dAlpha * i);
-			z = r * cos(dAlpha * i) * sin(dBeta * j);
-
-			glNormal3f(x / r, y / r, z / r);
-			glVertex3f(x, y, z);
-		}
-		glEnd();
-	}
+	m_eyex = cameraR * cos(alpha) * cos(beta);
+	m_eyey = cameraR * sin(alpha);
+	m_eyez = cameraR * cos(alpha) * sin(beta);
 }
 
-void CGLRenderer::PositionalLight1()
-{
-	float light_ambient[] = { 0.0, 1.0, 1.0, 1.0 };
-	float light_diffuse[] = { 0.0, 1.0, 1.0, 1.0 };
-	float light_specular[] = { 0.0, 1.0, 1.0, 1.0 };
 
-	GLfloat light_position[] = { 20, 15, 0,1 };
-
-	// boja i intenzitet svetlosti
-	glLightfv(GL_LIGHT2, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT2, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT2, GL_SPECULAR, light_specular);
-
-	glLightf(GL_LIGHT2, GL_SPOT_EXPONENT, 1.0f);
-
-	glLightfv(GL_LIGHT2, GL_POSITION, light_position);
-
-	GLfloat spot_direction[] = { -1, 0, 0 };
-	glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, spot_direction);
-	// slabljenje
-	glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 1.0);
-
-	// usmerenje izvora
-	glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 35);
-
-	if (normale) {
-		glPushMatrix();
-		m_blueLight.IzaberiMaterijal();
-		glTranslatef(30, 50, 0);
-		DrawSphere(1);
-		glPopMatrix();
-		glEnable(GL_LIGHT2);
-	}
-	else
-		glDisable(GL_LIGHT2);
-}
